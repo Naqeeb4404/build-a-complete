057bbb768c9a24da2152a7cd37f790b4ee133f8f
Naqeeb <naqeeblat@gmail.com>
2022-10-31 23:08:17 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="witdh=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1"
+    <! ===== Line awesome icons import ===== >
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+    <body>
+
+    <header>
+        <div class="logo">
+
+</div>
+<s></s>
+<ul>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul>
+<div class="more-links">
+</header>
+</body>
+
+</html>

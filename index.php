7923f0c3bbc727480ff1cd09cd245daef5c40cb9
Naqeeb <naqeeblat@gmail.com>
2022-11-01 21:57:55 +0800

    exercise baru

--- index.php
+++ index.php
@@ -12,6 +12,12 @@
 
     <header>
         <div class="logo">
+            <h2>
+                <span class="logo>
+                <span class="las la-fly"></span>
+</span>
+Oraimy
+            </h2>
 </div>
 <ul>
     <li><a href=""></a></li>
@@ -21,7 +27,39 @@
     <li><a href=""></a></li>
 </ul>
 <div class="more-links">
+    < href="" class="btn">Contact me</a>
 </header>
+
+<main>
+    <section class="hero">
+        <div class="hero-flex">
+            <div class="hero-desc">
+                <h1>John Smith <span class="las la-hand-spock"></span><h1>
+                    <div class="job-title">
+                        <span></span>
+                        <h3>Visual Designer</h3>
+</div>
+<div class="job-desc">
+    I'm a creative designer based in New York and I'm very passionate and dedicated to my work
+</div>
+<div class="hero-btns">
+    <a href="" class="btn">
+        Say Hello <span class="las la-arrow-up"></span>
+</a>
+</div>
+
+<div class="hero-img">
+<span>Follow me:</span>
+<div>
+    <span class="lab la-behance"></span>
+    <span class="lab la-dribble"></span>
+    <span class="lab la-instagram"></span>
+
+<img src="img/images.jpg" alt="">
+<img src="img/wel
+</div>
+</div>
+</section>
 </body>
 
 </html>

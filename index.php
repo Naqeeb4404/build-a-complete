60053379d47cbbbf5b39d6d9f6a1e20d31e23f00
Naqeeb <naqeeblat@gmail.com>
2022-11-01 22:11:11 +0800

    build complete 9:48

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="witdh=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1"
-    <! ===== Line awesome icons import ===== >
+    <! ==== Line awesome icons import ==== >
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="style.css">
     
@@ -20,11 +20,12 @@ Oraimy
             </h2>
 </div>
 <ul>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
+    <li><a href="" class="active">home</a></li>
+    <li><a href="">about</a></li>
+    <li><a href="">service</a></li>
+    <li><a href="">portfolio</a></li>
+    <li><a href="">blog</a></li>
+    <li><a href="">contact</a></li>
 </ul>
 <div class="more-links">
     < href="" class="btn">Contact me</a>
@@ -32,7 +33,8 @@ Oraimy
 
 <main>
     <section class="hero">
-        <div class="hero-flex">
+        <div class="container">
+
             <div class="hero-desc">
                 <h1>John Smith <span class="las la-hand-spock"></span><h1>
                     <div class="job-title">
@@ -48,18 +50,28 @@ Oraimy
 </a>
 </div>
 
-<div class="hero-img">
+<div class="hero-icons">
 <span>Follow me:</span>
 <div>
     <span class="lab la-behance"></span>
     <span class="lab la-dribble"></span>
     <span class="lab la-instagram"></span>
+</div>
+</div>
+</div>
 
+<div class="hero-img">
 <img src="img/images.jpg" alt="">
-<img src="img/wel
+<img src="img/logo.jpg" alt="">
+
 </div>
 </div>
 </section>
+<section class="about>
+
+
+</section>
+</main>
 </body>
 
 </html>
